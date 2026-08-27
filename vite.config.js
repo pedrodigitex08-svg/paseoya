6c1f1137c787412f4f2ee100720b6e3dfc17f928
pedrodigitex08-svg <pedrodigitex08@gmail.com>
2026-08-27 13:05:01 -0500

    feat: turn app into a Progressive Web App (PWA) with manifest and service worker

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg', 'icons.svg'],
+      manifest: {
+        name: 'PaseoYa',
+        short_name: 'PaseoYa',
+        description: 'La App para Organizar Viajes y Paseos Grupales.',
+        theme_color: '#F97316',
+        icons: [
+          {
+            src: 'icons.svg',
+            sizes: '192x192 512x512',
+            type: 'image/svg+xml',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
   ],
 })
